7bbe4ee0ceb3b41c950ccec22dd4a2084564d5c2
purg3me <jostaboi@protonmail.com>
2022-07-25 02:49:14 +0530

    Moved changes from script to patch file

--- setup.sh
+++ setup.sh
@@ -4,32 +4,10 @@
 echo "Cloning mediapipe repo..."
 git clone https://github.com/google/mediapipe.git
 
-# Setup for opencv4
-echo "Setting up opencv 4.x..."
-sed -i 's/#"include\/opencv4/"include\/opencv4/g' mediapipe/third_party/opencv_linux.BUILD
-
-# Edit the required files
-echo "Editing paths for models..."
-
-cd ./mediapipe
-path=$(pwd)
-cd ./mediapipe/modules/
-folders=$(find ./ -maxdepth 1 -type d | tail -n +2)
-nfolders=$(find ./ -maxdepth 1 -type d | tail -n +2 | wc -l)
-for d in $folders; do
-    cd $d
-    files=$(find ./ -name "*.pbtxt")
-    nfiles=$(find ./ -name "*.pbtxt" | wc -l)
-    for f in $files; do
-        test=$(grep 'model_path: "mediapipe' $f)
-        if [ "$test" != "" ]; then
-            sed -n 's/model_path: "mediapipe/model_path: "..\/mediapipe/gi' $f
-        fi
-    done
-    cd ..;
-done
-
-cd ../../../
+# Apply patch
+cd mediapipe
+git am ../0001-mediapipe_module.patch
+cd ..
 
 # Copy the project files
 echo "Copying project files..."
