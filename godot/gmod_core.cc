694f7a4fe94735325c6886a421f5e9e777198041
purg3me <jostaboi@protonmail.com>
2022-07-25 19:09:18 +0530

    Added methods

--- godot/gmod_core.cc
+++ godot/gmod_core.cc
@@ -32,38 +32,34 @@
 
 #include "gmod_core.h"
 
-// Variables to share data
-double** final_list = new double*[468];
-// bool running = false;
-bool grab_frames = false;
-// constexpr char graph_name[] = "";
-std::string graph_name;
+int face_landmarks_count = 468;
+int hand_right_landmarks_count = 21;
+int hand_left_landmarks_count = 21;
+int pose_landmarks_count = 33;
 
+// Variables to share data
+double** face_landmarks = new double*[face_landmarks_count];
+double** hand_right_landmarks = new double*[hand_right_landmarks_count];
+double** hand_left_landmarks = new double*[hand_left_landmarks_count];
+double** pose_landmarks = new double*[pose_landmarks_count];
+
+bool grab_frames = false;
+bool show_camera = false;
+bool show_detection = false;
+
+bool enable_face = false;
+bool enable_hand_right = false;
+bool enable_hand_left = false;
+bool enable_pose = false;
+
+std::string graph_name;
 
 constexpr char kInputStream[] = "input_video";
 constexpr char kOutputStream[] = "output_video";
 constexpr char kWindowName[] = "MediaPipe";
 
-// We are taking the input stream "DETECTIONS:output_detections" for "DetectionsToRenderDataCalculator"
-constexpr char kDetectionsStream[] = "face_landmarks";
-
-// ABSL_FLAG(std::string, calculator_graph_config_file, "mediapipe/graphs/hand_tracking/hand_tracking_desktop_live.pbtxt",
-ABSL_FLAG(std::string, calculator_graph_config_file, "/home/purg3me/Projects/MediapipeModule/mediapipe/mediapipe/graphs/holistic_tracking/holistic_tracking_cpu.pbtxt",
-          "Name of file containing text format CalculatorGraphConfig proto.");
-// ABSL_FLAG(std::string, calculator_graph_config_file, graph_name,
-//           "Name of file containing text format CalculatorGraphConfig proto.");
-ABSL_FLAG(std::string, input_video_path, "",
-          "Full path of video to load. "
-          "If not provided, attempt to use a webcam.");
-ABSL_FLAG(std::string, output_video_path, "",
-          "Full path of where to save result (.mp4 only). "
-          "If not provided, show result in a window.");
-
 absl::Status RunMPPGraph() {
   std::string calculator_graph_config_contents;
-  // MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
-  //     absl::GetFlag(FLAGS_calculator_graph_config_file),
-  //     &calculator_graph_config_contents));
   MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
       graph_name,
       &calculator_graph_config_contents));
@@ -77,59 +73,56 @@ absl::Status RunMPPGraph() {
   mediapipe::CalculatorGraph graph;
   MP_RETURN_IF_ERROR(graph.Initialize(config));
 
-  LOG(INFO) << "Initialize the camera or load the video.";
+  LOG(INFO) << "Initialize the camera";
   cv::VideoCapture capture;
-  const bool load_video = !absl::GetFlag(FLAGS_input_video_path).empty();
-  if (load_video) {
-    capture.open(absl::GetFlag(FLAGS_input_video_path));
-  } else {
-    capture.open(0);
-  }
+  capture.open(0);
   RET_CHECK(capture.isOpened());
 
-  cv::VideoWriter writer;
-  const bool save_video = !absl::GetFlag(FLAGS_output_video_path).empty();
-  if (!save_video) {
-    cv::namedWindow(kWindowName, /*flags=WINDOW_AUTOSIZE*/ 1);
+  cv::namedWindow(kWindowName, /*flags=WINDOW_AUTOSIZE*/ 1);
 #if (CV_MAJOR_VERSION >= 3) && (CV_MINOR_VERSION >= 2)
     capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
     capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
     capture.set(cv::CAP_PROP_FPS, 30);
 #endif
-  }
 
   LOG(INFO) << "Start running the calculator graph.";
   ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
                    graph.AddOutputStreamPoller(kOutputStream));
 
   // Added streampoller for Stream Detection
-  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller detection_poller,
-                   graph.AddOutputStreamPoller(kDetectionsStream));
-
-  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller presence_poller,
-                 graph.AddOutputStreamPoller("landmark_presence"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller face_detection_poller,
+                   graph.AddOutputStreamPoller("face_landmarks"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller face_presence_poller,
+                 graph.AddOutputStreamPoller("face_landmark_presence"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller hand_right_detection_poller,
+                   graph.AddOutputStreamPoller("hand_right_landmarks"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller hand_right_presence_poller,
+                 graph.AddOutputStreamPoller("hand_right_landmark_presence"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller hand_left_detection_poller,
+                   graph.AddOutputStreamPoller("hand_left_landmarks"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller hand_left_presence_poller,
+                 graph.AddOutputStreamPoller("hand_left_landmark_presence"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller pose_detection_poller,
+                   graph.AddOutputStreamPoller("pose_landmarks"));
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller pose_presence_poller,
+                 graph.AddOutputStreamPoller("pose_landmark_presence"));
 
   MP_RETURN_IF_ERROR(graph.StartRun({}));
 
   LOG(INFO) << "Start grabbing and processing frames.";
-  bool grab_frames = true;
+  grab_frames = true;
+  bool load_video = false;
   while (grab_frames) {
     // Capture opencv camera or video frame.
     cv::Mat camera_frame_raw;
     capture >> camera_frame_raw;
-    if (camera_frame_raw.empty()) {
-      if (!load_video) {
-        LOG(INFO) << "Ignore empty frames from camera.";
-        continue;
-      }
-      LOG(INFO) << "Empty frame, end of video reached.";
-      break;
-    }
+
+    LOG(INFO) << "Ignore empty frames from camera.";
+
     cv::Mat camera_frame;
     cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
-    if (!load_video) {
-      cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
-    }
+
+    cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
 
     // Wrap Mat into an ImageFrame.
     auto input_frame = absl::make_unique<mediapipe::ImageFrame>(
@@ -147,44 +140,94 @@ absl::Status RunMPPGraph() {
 
     // Get the graph result packet, or stop if that fails.
     mediapipe::Packet packet;
-    mediapipe::Packet detection_packet;
     mediapipe::Packet presence_packet;
+    mediapipe::Packet detection_packet;
 
     if (!poller.Next(&packet)) break;
-    // if (!poller.Next(&detection_packet)) break;
 
-    if (!presence_poller.Next(&presence_packet)) break;
-    auto is_landmark_present = presence_packet.Get<bool>();
-
-    if (is_landmark_present) {
-      if (!detection_poller.Next(&detection_packet)) break;
-      auto& output_detections = detection_packet.Get<mediapipe::NormalizedLandmarkList>();
-      for ( int i=0; i < 468; ++i){
-        const mediapipe::NormalizedLandmark& landmark = output_detections.landmark(i);
-        final_list[i] = new double[3];
-        final_list[i][0] = landmark.x();
-        final_list[i][1] = landmark.y();
-        final_list[i][2] = landmark.z();
+    if(enable_face){
+      // Check if landmark is present or not
+      if (!face_presence_poller.Next(&presence_packet)) break;
+      auto is_landmark_present = presence_packet.Get<bool>();
+      // Only get detection packets if landmark is detected, otherwise program crashes
+      if (is_landmark_present) {
+        if (!face_detection_poller.Next(&detection_packet)) break;
+        auto& output_detections = detection_packet.Get<mediapipe::NormalizedLandmarkList>();
+        for ( int i=0; i < face_landmarks_count; ++i){
+          const mediapipe::NormalizedLandmark& landmark = output_detections.landmark(i);
+          face_landmarks[i] = new double[3];
+          face_landmarks[i][0] = landmark.x();
+          face_landmarks[i][1] = landmark.y();
+          face_landmarks[i][2] = landmark.z();
+        }
+      }
+    }
+    if(enable_hand_right){
+      // Check if landmark is present or not
+      if (!hand_right_presence_poller.Next(&presence_packet)) break;
+      auto is_landmark_present = presence_packet.Get<bool>();
+      // Only get detection packets if landmark is detected, otherwise program crashes
+      if (is_landmark_present) {
+        if (!hand_right_detection_poller.Next(&detection_packet)) break;
+        auto& output_detections = detection_packet.Get<mediapipe::NormalizedLandmarkList>();
+        for ( int i=0; i < hand_right_landmarks_count; ++i){
+          const mediapipe::NormalizedLandmark& landmark = output_detections.landmark(i);
+          hand_right_landmarks[i] = new double[3];
+          hand_right_landmarks[i][0] = landmark.x();
+          hand_right_landmarks[i][1] = landmark.y();
+          hand_right_landmarks[i][2] = landmark.z();
+        }
+      }
+    }
+    if(enable_hand_left){
+      // Check if landmark is present or not
+      if (!hand_left_presence_poller.Next(&presence_packet)) break;
+      auto is_landmark_present = presence_packet.Get<bool>();
+      // Only get detection packets if landmark is detected, otherwise program crashes
+      if (is_landmark_present) {
+        if (!hand_left_detection_poller.Next(&detection_packet)) break;
+        auto& output_detections = detection_packet.Get<mediapipe::NormalizedLandmarkList>();
+        for ( int i=0; i < hand_left_landmarks_count; ++i){
+          const mediapipe::NormalizedLandmark& landmark = output_detections.landmark(i);
+          hand_left_landmarks[i] = new double[3];
+          hand_left_landmarks[i][0] = landmark.x();
+          hand_left_landmarks[i][1] = landmark.y();
+          hand_left_landmarks[i][2] = landmark.z();
+        }
+      }
+    }
+    if(enable_pose){
+      // Check if landmark is present or not
+      if (!pose_presence_poller.Next(&presence_packet)) break;
+      auto is_landmark_present = presence_packet.Get<bool>();
+      // Only get detection packets if landmark is detected, otherwise program crashes
+      if (is_landmark_present) {
+        if (!pose_detection_poller.Next(&detection_packet)) break;
+        auto& output_detections = detection_packet.Get<mediapipe::NormalizedLandmarkList>();
+        for ( int i=0; i < pose_landmarks_count; ++i){
+          const mediapipe::NormalizedLandmark& landmark = output_detections.landmark(i);
+          pose_landmarks[i] = new double[3];
+          pose_landmarks[i][0] = landmark.x();
+          pose_landmarks[i][1] = landmark.y();
+          pose_landmarks[i][2] = landmark.z();
+        }
       }
     }
 
+    // Only show camera output if enabled ( disabled by default )
+    if(show_camera){
+      auto& output_frame = packet.Get<mediapipe::ImageFrame>();
 
-    auto& output_frame = packet.Get<mediapipe::ImageFrame>();
+      // Convert back to opencv for display or saving.
+      cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_frame);
+      cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
 
-    // Convert back to opencv for display or saving.
-    cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_frame);
-    cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
-    if (save_video) {
-      if (!writer.isOpened()) {
-        LOG(INFO) << "Prepare video writer.";
-        writer.open(absl::GetFlag(FLAGS_output_video_path),
-                    mediapipe::fourcc('a', 'v', 'c', '1'),  // .mp4
-                    capture.get(cv::CAP_PROP_FPS), output_frame_mat.size());
-        RET_CHECK(writer.isOpened());
+      // Only show detections if enabled ( disabled by default )
+      if(show_detection){
+        cv::imshow(kWindowName, output_frame_mat);
+      } else {
+        cv::imshow(kWindowName, input_frame_mat);
       }
-      writer.write(output_frame_mat);
-    } else {
-      cv::imshow(kWindowName, output_frame_mat);
       // Press any key to exit.
       const int pressed_key = cv::waitKey(5);
       if (pressed_key >= 0 && pressed_key != 255) grab_frames = false;
@@ -192,17 +235,43 @@ absl::Status RunMPPGraph() {
   }
 
   LOG(INFO) << "Shutting down.";
-  if (writer.isOpened()) writer.release();
   MP_RETURN_IF_ERROR(graph.CloseInputStream(kInputStream));
   return graph.WaitUntilDone();
 }
 
 double ** return_face_trackers(){
-  return final_list;
+  return face_landmarks;
+}
+double ** return_hand_right_trackers(){
+  return hand_right_landmarks;
+}
+double ** return_hand_left_trackers(){
+  return hand_left_landmarks;
+}
+double ** return_pose_trackers(){
+  return pose_landmarks;
 }
 
-void stop(){
-  grab_frames = false;
+void toggle_face(bool x){
+  enable_face = x;
+}
+void toggle_hand_right(bool x){
+  enable_hand_right = x;
+}
+void toggle_hand_left(bool x){
+  enable_hand_left = x;
+}
+void toggle_pose(bool x){
+  enable_pose = x;
+}
+void toggle_camera(bool x){
+  if(show_camera && !x){
+    cv::destroyAllWindows();
+  }
+  show_camera = x;
+}
+void toggle_detection(bool x){
+  show_detection = x;
 }
 
 int start(const char* filename){
@@ -218,20 +287,10 @@ int start(const char* filename){
   return EXIT_SUCCESS;
 }
 
-void hello() {
-  printf("Hello World!");
+void stop(){
+  grab_frames = false;
 }
 
-// int test(int argc, char** argv) {
-int track() {
-  // google::InitGoogleLogging(argv[0]);
-  // absl::ParseCommandLine(argc, argv);
-  absl::Status run_status = RunMPPGraph();
-  if (!run_status.ok()) {
-    LOG(ERROR) << "Failed to run the graph: " << run_status.message();
-    return EXIT_FAILURE;
-  } else {
-    LOG(INFO) << "Success!";
-  }
-  return EXIT_SUCCESS;
+void hello() {
+  printf("Hello World!");
 }
