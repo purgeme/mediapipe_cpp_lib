d9c88e645455007f43fdfab509394cb4b1edd601
Parvesh Kumar <parveshkumarbaloda@gmail.com>
2022-10-29 22:19:07 +0530

    Fixed extract_graphs.py

--- extract_graphs.py
+++ extract_graphs.py
@@ -8,10 +8,14 @@ mediapipe_graphs_src =  os.path.join(current_dir, "mediapipe/mediapipe/graphs")
 mediapipe_graphs_dst =  os.path.join(current_dir, "mediapipe_graphs")
 
 print("##########")
-if os.path.isdir(mediapipe_graphs_dst):
+if os.path.isdir(mediapipe_graphs_dst) == True:
     inp = input("Graphs folder already exists, do you wish to replace it ? [y/N]: ")
-    if inp == "y" or inp == "Y":
-        os.rmdir(mediapipe_graphs_dst)
-        print("Copying graph files...")
-        shutil.copytree(mediapipe_graphs_src, mediapipe_graphs_dst)
+    if inp != "y" and inp != "Y":
+        print("##########")
+        exit()
+    else:
+        shutil.rmtree(mediapipe_graphs_dst)
+print("Copying graph files...")
+shutil.copytree(mediapipe_graphs_src, mediapipe_graphs_dst)
+
 print("##########")
